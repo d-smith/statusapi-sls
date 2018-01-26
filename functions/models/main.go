bd404d86254be9e3e59219540673514b1e2369b2
d-smith <doug.smith.mail@gmail.com>
2018-01-26 09:42:50 -0800

    Added model driving from jupyter

--- functions/models/main.go
+++ functions/models/main.go
@@ -4,13 +4,45 @@ import (
     "fmt"
     "github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"strings"
 )
+
+func handleGet(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{Body: "models get\n", StatusCode: 200}, nil
+}
+
+func handlePost(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{Body: "models post\n", StatusCode: 200}, nil
+}
+
+func handlePut(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{Body: "models put\n", StatusCode: 200}, nil
+}
+
+func handleOtherRequests(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{Body: "other\n", StatusCode: 200}, nil
+}
 
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("Received body: ", request.Body)
 
-	return events.APIGatewayProxyResponse{Body: "hi ya from models\n", StatusCode: 200}, nil
+	var response events.APIGatewayProxyResponse
+	var err error
+
+	method := strings.ToLower(request.HTTPMethod)
+	switch method {
+	case "get":
+		response, err = handleGet(request)
+	case "post":
+		response, err = handlePost(request)
+	case "put":
+		response, err = handlePut(request)
+	default:
+		response, err = handleOtherRequests(request)
+	}
+
+	return response, err
 }
 
 func main() {
