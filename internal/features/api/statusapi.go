9d71478a8c5a2fc9d7e5c913174801790cdec7fa
d-smith <doug.smith.mail@gmail.com>
2018-02-06 10:47:00 -0800

    Added post of events in integration test

--- internal/features/api/statusapi.go
+++ internal/features/api/statusapi.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	. "github.com/gucumber/gucumber"
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,36 @@ import (
 	"os"
 	"time"
 )
+
+func postEventsForModel(apiKey, apiEndpoint string) error {
+	eventPostEndpoint := fmt.Sprintf("https://%s/dev/status/api/v1/events", apiEndpoint)
+	log.Println("send to", eventPostEndpoint)
+	txnId := fmt.Sprintf("txn-%d", rand.Int())
+	for i := 0; i < 2; i += 1 {
+		payload := fmt.Sprintf(`{"txn_id":"%s","event_id":"%d","step":"s%d","step_state":"completed"}`, txnId, i, i+1)
+		log.Println("sending payload", payload)
+
+		req, err := http.NewRequest("POST", eventPostEndpoint, bytes.NewBuffer([]byte(payload)))
+		if !assert.Nil(T, err) {
+			return err
+		}
+		req.Header.Add("x-api-key", apiKey)
+
+		client := &http.Client{}
+		log.Println("post event")
+		resp, err := client.Do(req)
+		if !assert.Nil(T, err) {
+			log.Printf("error on event request: %s", err.Error())
+			return err
+		}
+
+		if !assert.Equal(T, http.StatusOK, resp.StatusCode) {
+			return errors.New(fmt.Sprintf("Unexcepted status code %d", resp.StatusCode))
+		}
+	}
+
+	return nil
+}
 
 func init() {
 
@@ -47,7 +78,8 @@ func init() {
 	})
 
 	And(`^some correlated events for the model$`, func() {
-		//T.Skip() // pending
+		//curl -H "x-api-key: XXXX" -XPOST -d '{"txn_id":"1a","event_id":"1","step":"s1","step_state":"completed"}' https://ENDPOINT/dev/status/api/v1/events
+		postEventsForModel(apiKey, apiEndpoint)
 	})
 
 	When(`^I retrieve the model state for the correlated events$`, func() {
